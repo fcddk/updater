0a2d3b1047fdb2f0fc49a9a6b76bfb3a5b035408
Winston Wu <50894443+fcddk@users.noreply.github.com>
2019-10-08 12:40:42 +0800

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -3,7 +3,13 @@
   
 set -e
 set -x
+#wget
+wget http://10.221.128.158:90/cms/agent/agent-updater
+wget http://10.221.128.158:90/cms/agent/agent-updater.service
 
+#mv
+mv agent-updater /usr/local/agent/agent-updater
+mv agent-updater.service /lib/systemd/system/agent-updater.service
 #Install
 systemctl daemon-reload
 systemctl enable agent-updater.service
